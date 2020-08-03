c8abd096f312dfeec559117a535bd275e0bc53d3
Michelle Chen <mich.chen.94@gmail.com>
2020-08-03 11:33:43 -0700

    add buttons to render diff data on current page

--- static/root.jsx
+++ static/root.jsx
@@ -6,7 +6,10 @@ function About() {
   return <div> A tiny react demo site </div>
 }
 
-
+// allows for reusability and just specify prop=whatever you want
+// props anything we pass in as object
+// if we did props.anythingIWant, and if an object does not have that then it just returns undefined, will not throw an error
+// jsx ignores "undefined" it just sees nothing so puts nothing
 function ShoppingListItems(props) {
   return <li> {props.item} </li>
 }
@@ -16,29 +19,59 @@ function ShoppingList() {
   return (
     <ul>
     
-      <ShoppingListItems item="milk" />
+      <ShoppingListItems item="milk" anythingIWant="muffin"/>
       <ShoppingListItems item="kombucha" />
       <ShoppingListItems item="pineapple" />
       <ShoppingListItems item="hot sauce" />
-      
+
     </ul>
   )
 }
 
 
 
+
+// high level component that will keep nesting components to render
 // App component renders my Homepage component using <component />
 // components rendering components
+// <Homepage /> is short cut for <Homepage></Homepage>
 function App() {
-  return (
-    // can only return one thing, so combine them into a single div
-    <div>
-      <Homepage />
-      <ShoppingList />
-      <About />
-    </div>
+  // keeps track where we currently are
+  const [nav, setNav] = React.useState('home')
+
+  if (nav == 'home') {
+    return (
+      <div>
+          <button onClick={() => setNav('home')}> Homepage </button>
+          <button onClick={() => setNav('about')}> About </button>
+          <Homepage></Homepage>
+      </div>
+      
+      )
+  }
+
+  if (nav == 'about') {
+    return (
+      <div>
+          <button onClick={() => setNav('home')}> Homepage </button>
+          <button onClick={() => setNav('about')}> About </button>
+          <About></About>
+          <ShoppingList></ShoppingList>
+      </div>
     )
+  }
 }
+//   return (
+//     // can only return one thing, so combine them into a single div
+//     <div>
+//           <button onClick={}> Homepage </button>
+//           <button onClick={}> About </button>
+//       <Homepage />
+//       <ShoppingList />
+//       <About />
+//     </div>
+//     )
+// }
 
 
 // render the function component App
